6ecf844a0b5372f913d0b09d0442a81789e6672d
Gary Rowe <gary.rowe@metoffice.gov.uk>
2015-05-19 16:34:51 +0100

    Add health check to start script

--- start-service.sh
+++ start-service.sh
@@ -5,9 +5,9 @@ echo 1. At the password prompt, enter the password for the secret key ring
 echo 2. Send the service to background with CTRL+Z
 echo 3. Restart the stopped job in the background by typing 'bg'
 echo 4. Exit from shell 
-echo 5. Verify service is still running by checking
-echo ..   TEST: http://localhost:7070/brit/public-key   or 
-echo ..   LIVE: https://multibit.org/brit/public-key
+echo 5. Verify service as follows:
+echo ..   curl -XGET http://localhost:7071/healthcheck (check for no ERROR)
+echo ..   lynx https://multibit.org/brit/public-key (check for PGP key)
 echo ..
 echo TIP: You can find this process again by typing 'ps -A | grep brit'
 echo TIP: If 'NoClassDefFoundError: ... BouncyCastleProvider' copy bcprov-jdk16-1.46.jar to project root
