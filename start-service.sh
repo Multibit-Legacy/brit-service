ac828e679dc1db91a2f8367ac069546f0a2af8ca
Jim Burton <jim618@fastmail.co.uk>
2015-05-20 12:51:44 +0100

    #4 updated startup script

--- start-service.sh
+++ start-service.sh
@@ -6,9 +6,9 @@ echo 2. Send the service to background with CTRL+Z
 echo 3. Restart the stopped job in the background by typing 'bg'
 echo 4. Exit from shell 
 echo 5. Verify service as follows:
-echo ..   curl -XGET http://localhost:7071/healthcheck (check for no ERROR)
-echo ..   lynx https://multibit.org/brit/public-key (check for PGP key)
+echo ..   curl -XGET http://localhost:7071/healthcheck    -- Check for no ERROR --
+echo ..   lynx https://multibit.org/brit/public-key       -- Check for PGP key --
 echo ..
 echo TIP: You can find this process again by typing 'ps -AF | grep brit'
 echo TIP: If 'NoClassDefFoundError: ... BouncyCastleProvider' copy bcprov-jdk16-1.46.jar to project root
-java -cp "bcprov-jdk16-1.46.jar;target/brit-service-develop-SNAPSHOT.jar" org.multibit.hd.brit_server.BritService server config.yml
+java -cp "bcprov-jdk16-1.46.jar:target/brit-service-develop-SNAPSHOT.jar" org.multibit.hd.brit_server.BritService server config.yml
